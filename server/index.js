6a9ee9827c9361f3c8de3b9e9c2e45531305dca1
mattiaferrari <mattia2002.ferrari@gmail.com>
2024-01-25 15:43:35 +0100

    add first endpoint and .env

--- server/index.js
+++ server/index.js
@@ -1,5 +1,38 @@
 import express from "express"
+import "dotenv/config"
+import axios from "axios"
+import cors from "cors"
+
 const app = express()
+app.use(cors())
+app.use(express.json())
+
+const SELLIX_BASE_URL = "https://dev.sellix.io/v1"
+
+const axiosInstance = axios.create({
+  baseURL: SELLIX_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${process.env.SELLIX_API_KEY}`,
+  }
+})
+
+const users = []
+
+app.post("/createMerchant", async(req, res) => {
+  const sellixRes = await axiosInstance.post("/wallet/merchant", {
+    email: req.body.email,
+    name: req.body.name,
+    custom_fields: req.body.custom_fields ?? null
+  })
+  if (sellixRes.data.status !== 200) {
+    res.statusCode = sellixRes.data.status
+    res.json(sellixRes.data)
+  } else {
+    users.push(sellixRes.data.data.merchant)
+    res.statusCode = 200
+    res.json(sellixRes.data.data.merchant)
+  }
+})
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
